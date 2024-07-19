84450c197206edd659dc97d4cc1b5bebd7fa722d
bilaljulian <bilaljulian05@gmail.com>
2024-07-18 19:00:12 -0700

    Create next-env.d.ts

---
+++ next-env.d.ts
@@ -0,0 +1,9 @@
+/// <reference types="next" />
+/// <reference types="next/types/global" />
+
+declare module '*.css';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.png';
+declare module '*.gif';
+declare module '*.svg';
