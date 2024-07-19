7f87dd0ab24bbb8b6bd4972f6bffb049c3a414c2
bilaljulian <bilaljulian05@gmail.com>
2024-07-18 22:19:02 -0700

    Update setup.mjs

--- setup.mjs
+++ setup.mjs
@@ -1,11 +1,10 @@
-import { BookType, PrismaClient } from '@prisma/client';
-
+import { ItemType, PrismaClient } from '@prisma/client';
 import dotenv from 'dotenv';
 import { faker } from '@faker-js/faker';
 
 dotenv.config();
 
-const { TIDB_USER, TIDB_PASSWORD, TIDB_HOST, TIDB_PORT, TIDB_DB_NAME = 'bookshop', DATABASE_URL } = process.env;
+const { TIDB_USER, TIDB_PASSWORD, TIDB_HOST, TIDB_PORT, TIDB_DB_NAME = 'enullshop', DATABASE_URL } = process.env;
 // Notice: When using TiDb Cloud Serverless Tier, you **MUST** set the following flags to enable tls connection.
 const SSL_FLAGS = 'pool_timeout=60&sslaccept=accept_invalid_certs';
 // TODO: When TiDB Cloud support return DATABASE_URL, we can remove it.
@@ -36,10 +35,10 @@ const setup = async () => {
 
     // Seed data.
     const users = await seedUsers(client, 20);
-    const authors = await seedAuthors(client, 20);
-    const books = await seedBooks(client, 100);
-    await seedBooksAndAuthors(client, books, authors);
-    await seedRatings(client, books, users);
+    const sellers = await seedSellers(client, 20);
+    const items = await seedItems(client, 100);
+    await seedItemsAndSellers(client, items, sellers);
+    await seedReviews(client, items, users);
   } catch (error) {
     throw error;
   } finally {
@@ -53,12 +52,12 @@ const setup = async () => {
 async function seedUsers(client, num) {
   const records = [...Array(num)].map((value, index) => {
     const id = index + 1;
-    const nickname = faker.internet.userName();
+    const username = faker.internet.userName();
     const balance = faker.random.numeric(6);
 
     return {
       id,
-      nickname,
+      username,
       balance
     };
   });
@@ -75,117 +74,113 @@ async function seedUsers(client, num) {
   return records;
 }
 
-// Seed authors data.
-async function seedAuthors(client, num) {
+// Seed sellers data.
+async function seedSellers(client, num) {
   const records = [...Array(num)].map((value, index) => {
     const id = index + 1;
     const name = faker.name.fullName();
-    const gender = faker.datatype.boolean();
-    const birthYear = faker.datatype.number({ min: 1900, max: 2000 });
-    let deathYear = birthYear + faker.datatype.number({ min: 20, max: 100 });
-    if (deathYear > 100) {
-      deathYear = undefined;
-    }
+    const location = faker.address.city();
+    const rating = faker.datatype.number({ min: 1, max: 5 });
+
     return {
       id,
       name,
-      gender,
-      birthYear,
-      deathYear
+      location,
+      rating
     };
   });
 
-  const added = await client.author.createMany({
+  const added = await client.seller.createMany({
       data: records,
       skipDuplicates: true
   });
 
   if (added.count > 0) {
-    console.log(`Successfully inserted ${added.count} author records.`);
+    console.log(`Successfully inserted ${added.count} seller records.`);
   }
 
   return records;
 }
 
-// Seed books data.
-const bookTypes = Object.keys(BookType);
-async function seedBooks(client, num) {
+// Seed items data.
+const itemTypes = Object.keys(ItemType);
+async function seedItems(client, num) {
   const records = [...Array(num)].map((value, index) => {
     const id = index + 1;
-    const title = faker.music.songName();
-    const bookTypeIndex = faker.datatype.number({ min: 0, max: bookTypes.length - 1 });
-    const type = bookTypes[bookTypeIndex];
-    const publishedAt = faker.date.between('2000-01-01T00:00:00.000Z', Date.now().toString());
+    const name = faker.commerce.productName();
+    const itemTypeIndex = faker.datatype.number({ min: 0, max: itemTypes.length - 1 });
+    const type = itemTypes[itemTypeIndex];
+    const createdAt = faker.date.between('2000-01-01T00:00:00.000Z', Date.now().toString());
     const stock = faker.datatype.number({ min: 0, max: 200 });
     const price = faker.datatype.number({ min: 0, max: 200, precision: 0.01 });
 
     return {
       id,
-      title,
+      name,
       type,
-      publishedAt,
+      createdAt,
       stock,
       price
     };
   });
 
-  const added = await client.book.createMany({
+  const added = await client.item.createMany({
       data: records,
       skipDuplicates: true
   });
 
   if (added.count > 0) {
-    console.log(`Successfully inserted ${added.count} book records.`);
+    console.log(`Successfully inserted ${added.count} item records.`);
   }
 
   return records;
 }
 
-// Seed books and authors data.
-async function seedBooksAndAuthors(client, books, authors) {
-  const records = books.map((book) => {
-    const authorIndex = faker.datatype.number({ min: 0, max: authors.length - 1 });
-    const author = authors[authorIndex];
+// Seed items and sellers data.
+async function seedItemsAndSellers(client, items, sellers) {
+  const records = items.map((item) => {
+    const sellerIndex = faker.datatype.number({ min: 0, max: sellers.length - 1 });
+    const seller = sellers[sellerIndex];
 
     return {
-      bookId: book.id,
-      authorId: author.id
+      itemId: item.id,
+      sellerId: seller.id
     }
   });
 
-  const added = await client.bookAuthor.createMany({
+  const added = await client.itemSeller.createMany({
     data: records,
     skipDuplicates: true
   });
 
   if (added.count > 0) {
-    console.log(`Successfully inserted ${added.count} book and author relation records.`);
+    console.log(`Successfully inserted ${added.count} item and seller relation records.`);
   }
 
   return records;
 }
 
-// Seed ratings data.
-async function seedRatings(client, books, users) {
+// Seed reviews data.
+async function seedReviews(client, items, users) {
   let total = 0;
-  for (const book of books) {
-    const ratingNum = faker.datatype.number({ min: 10, max: 30});
-    const bookId = book.id;
-    const records = [...Array(ratingNum)].map(() => {
-      const score = faker.datatype.number({ min: 1, max: 5 });
+  for (const item of items) {
+    const reviewNum = faker.datatype.number({ min: 10, max: 30});
+    const itemId = item.id;
+    const records = [...Array(reviewNum)].map(() => {
+      const rating = faker.datatype.number({ min: 1, max: 5 });
       const userIndex = faker.datatype.number({ min: 1, max: users.length - 1 });
       const userId = users[userIndex].id;
-      const ratedAt = faker.date.between(book.publishedAt.toString(), Date.now().toString());
+      const reviewedAt = faker.date.between(item.createdAt.toString(), Date.now().toString());
 
       return {
         userId,
-        bookId,
-        score,
-        ratedAt
+        itemId,
+        rating,
+        reviewedAt
       }
     });
 
-    const added = await client.rating.createMany({
+    const added = await client.review.createMany({
       data: records,
       skipDuplicates: true
     });
@@ -194,7 +189,7 @@ async function seedRatings(client, books, users) {
   }
 
   if (total > 0) {
-    console.log(`Successfully inserted ${total} rating records.`);
+    console.log(`Successfully inserted ${total} review records.`);
   }
 }
 
@@ -206,3 +201,4 @@ try {
 }
 
 export { setup };
+
